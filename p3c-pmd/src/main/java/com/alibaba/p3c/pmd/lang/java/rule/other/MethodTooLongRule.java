bf4237c59d47d99e4a652ea82b2b0da6fe52fc4c
herandingyi <582866652@qq.com>
2023-12-15 18:34:19 +0800

    remove too long rule

--- p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/other/MethodTooLongRule.java
+++ p3c-pmd/src/main/java/com/alibaba/p3c/pmd/lang/java/rule/other/MethodTooLongRule.java
@@ -54,100 +54,13 @@ public class MethodTooLongRule extends AbstractAliRule {
     /**
      * sortedMap will be reinitialized for each source file.
      */
-    private SortedMap<Integer, Node> sortedNodeAndComment;
-
     @Override
     public Object visit(ASTCompilationUnit cUnit, Object data) {
-        sortedNodeAndComment = orderedCommentsAndExpressions(cUnit);
         return super.visit(cUnit, data);
     }
 
     @Override
     public Object visit(ASTMethodDeclaration node, Object data) {
-        // Include method modifiers.
-        ASTClassOrInterfaceBodyDeclaration classOrInterfaceBodyDecl =
-            (ASTClassOrInterfaceBodyDeclaration)node.jjtGetParent();
-
-        int startLine = classOrInterfaceBodyDecl.getBeginLine();
-        int endLine = classOrInterfaceBodyDecl.getEndLine();
-
-        Node firstChild = classOrInterfaceBodyDecl.jjtGetChild(0);
-        // Method has annotation
-        if (firstChild instanceof ASTAnnotation) {
-            Token firstToken = (Token)classOrInterfaceBodyDecl.jjtGetFirstToken();
-            // If annotation is before modifier, exclude the annotation.
-            if (ANNOTATION_PREFIX.equals(firstToken.image)) {
-                ASTAnnotation annotation = (ASTAnnotation)firstChild;
-                Token lastToken = (Token)annotation.jjtGetLastToken();
-
-                // First token after annotation. The same line or next line after annotation.
-                Token next = lastToken.next;
-                startLine = next.beginLine;
-            }
-        }
-
-        // Get comment line count.
-        int commentLineCount = getCommentLineCount(node);
-
-        if (endLine - startLine - commentLineCount + 1 > MAX_LINE_COUNT) {
-            ViolationUtils.addViolationWithPrecisePosition(this, node, data,
-                I18nResources.getMessage("java.other.MethodTooLongRule.violation.msg", node.getName()));
-        }
         return data;
-    }
-
-    /**
-     * Order comments and expressions.
-     *
-     * @param cUnit compilation unit
-     * @return sorted comments and expressions
-     */
-    protected SortedMap<Integer, Node> orderedCommentsAndExpressions(ASTCompilationUnit cUnit) {
-
-        SortedMap<Integer, Node> itemsByLineNumber = new TreeMap<>();
-
-        // expression nodes
-        List<ASTExpression> expressionNodes = cUnit.findDescendantsOfType(ASTExpression.class);
-        NodeSortUtils.addNodesToSortedMap(itemsByLineNumber, expressionNodes);
-
-        NodeSortUtils.addNodesToSortedMap(itemsByLineNumber, cUnit.getComments());
-
-        return itemsByLineNumber;
-    }
-
-    /**
-     * Get number of comment lines
-     *
-     * @param methodDecl
-     * @return
-     */
-    private int getCommentLineCount(ASTMethodDeclaration methodDecl) {
-        int lineCount = 0;
-        AbstractJavaNode lastNode = null;
-
-        for (Entry<Integer, Node> entry : sortedNodeAndComment.entrySet()) {
-            Node value = entry.getValue();
-            if (value.getBeginLine() <= methodDecl.getBeginLine()) {
-                continue;
-            }
-            if (value.getBeginLine() > methodDecl.getEndLine()) {
-                break;
-            }
-
-            // value should be either expression or comment.
-            if (value instanceof AbstractJavaNode) {
-                lastNode = (AbstractJavaNode)value;
-            } else if (value instanceof FormalComment || value instanceof MultiLineComment) {
-                Comment comment = (Comment)value;
-                lineCount += comment.getEndLine() - comment.getBeginLine() + 1;
-            } else if (value instanceof SingleLineComment) {
-                SingleLineComment singleLineComment = (SingleLineComment)value;
-                // Comment may in the same line with node.
-                if (lastNode == null || singleLineComment.getBeginLine() != lastNode.getBeginLine()) {
-                    lineCount += 1;
-                }
-            }
-        }
-        return lineCount;
     }
 }
